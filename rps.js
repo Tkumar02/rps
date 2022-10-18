bc4d4dc357a142dc13b369b0d918ea96dcc1d013
TKumar02 <ta_265@yahoo.co.uk>
2022-10-18 13:36:44 +0100

    changed UI

--- rps.js
+++ rps.js
@@ -1,132 +1,70 @@
-console.log('Welcome to Rock, Paper, Scissors')
-const computer = document.querySelector('.computer');
-const player = document.getElementById('player');
-const button = document.getElementById('play');
-const enter = document.querySelector('.enter');
-const outcome = document.querySelector('.outcome');
-const playerChoice = document.querySelector('.playerChoice');
+
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const userChoice = document.querySelector('.userChoice')
+const choices = document.querySelectorAll('.choices')
+const computerChoice = document.querySelector('.computerChoice')
+const result = document.querySelector('.result')
+let playerWin = 0;
+let computerWin = 0;
+const pScore = document.querySelector('.pScore')
+const cScore = document.querySelector('.cScore')
 const final = document.querySelector('.final')
 
 function getComputerChoice(){
     let rpsArray = ["rock","paper","scissors"];
     let randomElement = rpsArray[Math.floor(Math.random() * rpsArray.length)];
-    return randomElement ;
+    computerChoice.textContent = randomElement;
 }
 
-// function playerSelection(){
-//     let x =  prompt("Rock, paper or scissors? Select your weapon of choice: ");
-//     let y = x.toLowerCase(x);
-//     return y; 
-// }
-
-function playerSelection(){
-    let y = player.value;
-    return y;
-}
-
-function playARound() {
-    let x = getComputerChoice();
-    let y = playerSelection();
-    if (x==y) { 
-        return "you tie!";
-    }
-    else if (x=='rock' && y == 'scissors') {
-        return "rock beats scissors, you lose!"
-    }
-
-    else{
-        return (`you played ${y} and the computer played ${x}`);
-    }
-}
-
-// function playRound(){
-//     let x = getComputerChoice()
-//     let y = playerSelection()
-//     switch(true){
-//         case x == y:
-//             return ('tie');
-//             break;
-//         case x == 'paper' && y == 'rock':
-//         case x == 'rock' && y == 'scissors':
-//         case x == 'scissors' && y == 'paper':
-//             // return (`Computer chose ${x}, you lose`);
-//             return ('loss')
-//             break;
-//         default:
-//             // return (`Computer chose ${x}, you win!`);
-//             return ('win')
-//     }
-// }
+choices.forEach(choice=>{
+    choice.addEventListener('click',function(e){
+        userChoice.textContent = e.target.id
+    })
+})
 
 function playRound(){
-    let x = getComputerChoice();
-    let y = playerSelection();
-    computer.textContent = x;
-    playerChoice.textContent = y;
-    switch(true){
-        case x == y:
-            // console.log(`Computer also chose ${x}`)
-            outcome.textContent = `Both chose ${x}`;
-            final.textContent = 'TIE';
-            final.style.background = 'yellow';
-            return ('tie');
-            break;
-        case x == 'paper' && y == 'rock':
-        case x == 'rock' && y == 'scissors':
-        case x == 'scissors' && y == 'paper':
-            // console.log (`Computer chose ${x} and you chose ${y}`);
-            outcome.textContent = `${x} beats ${y}`
-            final.textContent = "You Lose!";
-            final.style.background = "red";
-            return ('loss')
-            break;
-        case y == 'paper' && x == 'rock':
-        case y == 'rock' && x == 'scissors':
-        case y == 'scissors' && x == 'paper':
-            outcome.textContent = `${y} beats ${x}`;
-            final.textContent = "You Win!!";
-            final.style.background = 'green';
-            return ('win')
-            break;
-        // default:
-        //     console.log('Please choose again');
-        //     playRound();
-    }
-}
+    getComputerChoice();
+    let playerSelection = userChoice.textContent;
+    let computerSelection = computerChoice.textContent;
+    final.textContent = ""
 
-function game(){
-    let win = 0;
-    for (let i=1; i<=5; i++) {
-        let x = playRound();
-        if (x == 'win') {
-            // console.log(`you have won round ${i}/5`);
-            outcome.textContent = `you have won round ${i}/5`
-            win ++;
-        }
-        else if (x == 'loss'){
-            console.log(`You lost round ${i}/5`)
-        }
-        else if (x=='tie'){
-            console.log(`Round ${i}/5 is a tie`)
-        }
-        else{
-            console.log(`You chose a non existent weapon, you lose round ${i}/5`);
-        }
+    if (playerSelection == computerSelection){
+        result.textContent = "It's a Tie";
     }
-    if (win>2){
-        return (`You have won ${win} rounds in total and you have beaten the computer`);
-    } 
-    else if (win<3 && win>0){
-        return (`You have only won ${win} of 5 rounds, and you have lost`);
+    else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper') || (playerSelection ==  'paper' && computerSelection == 'rock')){
+        result.textContent = "You Win!"
+        playerWin += 1;
     }
     else{
-        return ("You haven't won any round, and you have lost against the computer")
+        result.textContent = "You lose :("
+        computerWin +=1;
+    }
+    pScore.textContent = playerWin;
+    cScore.textContent = computerWin;
+    if (computerWin==5&&computerWin>playerWin){
+        final.textContent = "COMPUTER WINS (select rock, paper or scissors to start a new game)";
+        computerWin = 0;
+        playerWin = 0;
+        return
+    }
+    else if(playerWin==5 && playerWin>computerWin){
+        final.textContent = "YOU WIN! (select rock, paper or scissors to start a new game)";
+        computerWin = 0;
+        playerWin = 0;
+        return
     }
 }
 
-
-
-button.addEventListener('click',playRound);
-enter.addEventListener('click', e=>{
-    playerChoice.textContent = player.value
+choices.forEach(choice=>{
+    choice.addEventListener('click',playRound)
 })
+
+
+
+
+
+
+
+
